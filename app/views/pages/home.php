39d4a7fe9ea3f59db0ebb9d413762d0525441050
ClaudiaC18 <claudia.capuana@gmail.com>
2025-11-18 13:14:34 +0100

    Traduction des helpers en Helpers

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -2,7 +2,8 @@
 
 <ul>
 <?php foreach($books as $book): ?>
-    <li><?php echo $book['title']; ?>
-<?php echo $book['resume']; ?></li>
+    <li><?php echo $book['title']; ?> - 
+    <!-- on va tronquer le resume -->
+<?php echo \Core\Helpers::truncate( $book['resume'],30); ?></li>
 <?php endforeach; ?>
 </ul>
